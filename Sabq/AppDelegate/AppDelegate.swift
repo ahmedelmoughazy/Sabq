78e63347b132ddb2a6f7d88f736970ee536b6154
Ahmed Refaat <ahmed.refaat@Users-Mac-mini.local>
2019-10-17 14:54:31 +0200

    basic structure done

---
+++ Sabq/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Sabq
+//
+//  Created by Ahmed Refaat on 10/17/19.
+//  Copyright © 2019 Ahmed Refaat. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
